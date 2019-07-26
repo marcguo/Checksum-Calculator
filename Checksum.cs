13e2f2f83b1077f4eecd5d26108ad98758c3b5db
Marc Guo <37849300+marcguo@users.noreply.github.com>
2019-07-26 07:16:51 -0400

    Update Checksum.cs
    Added comments.

--- Checksum.cs
+++ Checksum.cs
@@ -5,12 +5,20 @@ namespace Binary_File_Checksum_Calculator
 {
     class Program
     {
+        /// <summary>
+        /// Program entry.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            // Keep performing checksum calculation for different files until the user closes the program.
             while (true)
             {
+                // Let the user choose between checksum for one file or compare two files to find the
+                // "diverge" point, meaning the bit number where two files become different at.
                 Console.WriteLine("Please select between sum and com: \n");
                 string selection = Console.ReadLine();
+                // Compare two files to find the diverge point:
                 if (selection == "com")
                 {
                     Console.WriteLine("Please enter the full path of the first file you want to read: \n");
@@ -19,59 +27,92 @@ namespace Binary_File_Checksum_Calculator
                     Console.WriteLine("Please enter the full path of the second file you want to read: \n");
                     string secondFileName = Console.ReadLine();
                     byte[] secondFileBytes = File.ReadAllBytes(secondFileName);
+                    // The current lower bound of the area of interest.
                     int currentLow = 0;
+                    // The current upper bound of the area of interest.
                     int currentHigh = firstFileBytes.Length;
+                    // Similar to the pivot point in a binary search.
                     int currentPivot = (currentLow + currentHigh) / 2;
+                    // The old cutoff point, the byte number where we stop comparing two files' checksum at.
                     int oldPivot = 0;
+                    // Before we find out the "diverge" point of these two files:
                     while (oldPivot != currentPivot)
                     {
+                        // Update the old pivot point.
                         oldPivot = currentPivot;
-                        currentPivot = NextPivot(currentPivot, IsSameCheckSum(currentPivot, firstFileBytes, secondFileBytes),
+                        // Assign a new value to the current pivot point, depending on whether the files are
+                        // still the same in checksum or not.
+                        currentPivot = 
+                            NextPivot(currentPivot, IsSameCheckSum(currentPivot, firstFileBytes, secondFileBytes),
                             currentLow, currentHigh);
+                        // Update the bounds accordingly.
                         currentHigh = currentPivot > oldPivot ? currentHigh : oldPivot;
-                        currentLow = currentPivot < oldPivot ? currentLow : oldPivot;
+                        currentLow  = currentPivot < oldPivot ? currentLow : oldPivot;
                     }
                     Console.WriteLine("Go to byte # " + currentPivot.ToString() + "\n");
                 }
+                // Or just calculate the checksum for one file:
                 else if (selection == "sum")
                 {
                     Console.WriteLine("Please enter the full path of the file you want to read: \n");
                     string fileName = Console.ReadLine();
                     byte[] fileBytes = File.ReadAllBytes(fileName);
                     int checkSum = CalculateCheckSum(fileBytes);
-                    Console.WriteLine("Check sum is: ");
+                    Console.WriteLine("Checksum is: ");
                     Console.WriteLine(checkSum.ToString());
                 }
             }
-
-            
         }
 
-        static bool IsSameCheckSum(int cutoff, byte[] firstFileBytes, byte[] secondFileBytes)
+        /// <summary>
+        /// Compares two files from the beginning up to the cutoff bit and determins if these two 
+        /// files have the same checksum from the beginning to the cutoff point.
+        /// </summary>
+        /// <param name="cutoff">The cutoff point to stop comparison at.</param>
+        /// <param name="firstFileBytes">The first file to compare, represented in byte array format.</param>
+        /// <param name="secondFileBytes">The second file to compare, represented in byte array format.</param>
+        /// <returns></returns>
+        private static bool IsSameCheckSum(int cutoff, byte[] firstFileBytes, byte[] secondFileBytes)
         {
-            int firstCheckSum = 0;
+            // Places to store the current checksum of the files.
+            int firstCheckSum  = 0;
             int secondCheckSum = 0;
             for (int i = 0; i < cutoff; i++)
             {
-                firstCheckSum += firstFileBytes[i];
+                firstCheckSum  += firstFileBytes[i];
                 secondCheckSum += secondFileBytes[i];
             }
             return firstCheckSum == secondCheckSum;
         }
 
-        static int NextPivot(int currentPivot, bool isSame, int currentLow, int currentHigh)
+        /// <summary>
+        /// Finds the next pivot point (the point to stop comparing checksum at for the files).
+        /// </summary>
+        /// <param name="currentPivot">The current pivot point, or the cutoff point.</param>
+        /// <param name="isSame">Are the two files' checksums the same so far?</param>
+        /// <param name="currentLow">The current lower bound of the area of interest.</param>
+        /// <param name="currentHigh">The current upper bound of the area of interest.</param>
+        /// <returns></returns>
+        private static int NextPivot(int currentPivot, bool isSame, int currentLow, int currentHigh)
         {
+            // If two files have the same checksum so far, move forward.
             if (isSame)
             {
                 return (currentPivot + currentHigh) / 2;
             }
+            // Else. backtrack to find out where things became different.
             else
             {
                 return (currentPivot + currentLow) / 2;
             }
         }
 
-        static int CalculateCheckSum(byte[] bytes)
+        /// <summary>
+        /// Calculates the checksum for the given byte array.
+        /// </summary>
+        /// <param name="bytes">The byte array to perform checksum on.</param>
+        /// <returns>Sum of all bytes' values.</returns>
+        private static int CalculateCheckSum(byte[] bytes)
         {
             int checkSum = 0;
             foreach (byte b in bytes)
